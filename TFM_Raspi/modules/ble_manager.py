cd9dff1e3700b8382ca6f805102bd4223a81ade1
DanielOlssonAndres <daniel.olsson@alumnos.upm.es>
2026-01-13 13:19:22 +0100

    correcion de direccion de caracteristica

--- TFM_Raspi/modules/ble_manager.py
+++ TFM_Raspi/modules/ble_manager.py
@@ -4,7 +4,7 @@ from functools import partial
 from bleak import BleakClient, BleakScanner
 from modules.data_handler import decode_packet
 
-CHARACTERISTIC_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+CHARACTERISTIC_UUID = "0000FF01-0000-1000-8000-00805F9B34FB"
 
 class BLEManager:
     def __init__(self):
